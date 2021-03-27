dca0a7132cbea209cfb0e228329e1adc3f5f1ab1
ivo94 <begonjaivo@yahoo.com.ar>
2021-03-27 17:38:41 -0300

    Cambios

--- finance.py
+++ finance.py
@@ -17,39 +17,6 @@ from random import random
 from math import log
 import statistics
 
-#Generates a sample from a normal random variable
-#sample_array = np.random.normal(0,1,10)
-#sample = list(sample_array)
-#print(sample)
-
-# =============================================================================
-# Samples
-# =============================================================================
-#Normal
-#sample_normal = np.random.normal(0, 1, 1000)
-#Uniform [0,1)
-def sample_uniform(size):
-    seed(random())
-    res = []
-    for i in range(0,size):
-        res = res + [random()]
-    return res
-
-#Testing a sample against the normal distribution with a given significance level
-#H0: the sample is normally distributed with mean 0 and variance 1
-#H1: the sample does not come from a normal(0,1)
-def normalTest(sample, level):
-    res = kstest(sample,'norm')
-    if res[1] < level:
-        print('null hypothesis rejected')
-        print(f"p-value = {res[1]}")
-    else:
-        print('null hypothesis not rejected')
-        print(f"p-value = {res[1]}")
-        
-#normalTest(sample_normal,0.05)
-#normalTest(sample_uniform(50),0.05)
-
 #Determines how many prices are >= than a particular price 
 def empiricDistribution(prices, price):
     counter = 0
@@ -89,7 +56,7 @@ def buyAndHold(stock, sYear, sMonth, sDay, eYear, eMonth, eDay):
 # -sampleSize is the amount of prices taken to calculate the cumulative probability      
 # -stock is the name of the stock whose historic prices are analyzed
 # -prob is the probability that the values in the sample are <= than the price being considered
-# -sYear, sMonth, sDay, eYear, eMonth, eDay are the startinga and ending year, month and day, respectively, where the analysis begins 
+# -sYear, sMonth, sDay, eYear, eMonth, eDay are the starting and ending year, month and day, respectively, where the analysis begins 
 def system(sampleSize, stock, prob, sYear, sMonth, sDay, eYear, eMonth, eDay):
     df = web.DataReader(stock, 'yahoo', dt.datetime(sYear, sMonth, sDay), dt.datetime(eYear, eMonth, eDay))
     prices = [] 
@@ -100,7 +67,7 @@ def system(sampleSize, stock, prob, sYear, sMonth, sDay, eYear, eMonth, eDay):
     while i < len(prices):
         sample1 = prices[i+1-sampleSize:i+1]
         cumulative = empiricDistribution(sample1, sample1[sampleSize-1])
-        if cumulative <= prob: #ACA DEBE INGRESAR IGUAL EL PUNTO DE COMPRA Y SI NO ENCUENTRA PUNTO DE VENTA DEBE SER CONSIDERADO PERDIDA
+        if cumulative <= prob:
             for j in range(i+1, len(prices)):
                 sample2 = prices[j+1-sampleSize:j+1]
                 cumulative = empiricDistribution(sample2, sample2[sampleSize-1])
@@ -197,57 +164,6 @@ def TEST_NASDAQ_0():
         
         #print the output to a file by doing "python finance.py > results.txt (or any name)
 
+#Choose a test
 #TEST_MERVAL_0()
 TEST_NASDAQ_0()
-
-
-#try:
-#    for i in range(len(prices)):
-#        prices[i] = log(prices[i])
-#except:
-#    ValueError: 'log of 0'
-#    
-#mean = statistics.mean(prices)
-#std = statistics.stdev(prices)  
-
-#for i in range(len(prices)):
-#    prices[i] = (prices[i]-mean)/std**2
-
-#prices_normalized = [0 for x in range(len(prices))]
-#for i in range(len(prices)):
-#    prices_normalized[i] = (prices[i]-mean)/(std**2)
-   
-#normalTest(prices, 0.05)
-#plt.plot(prices)
-#plt.ylabel('Price')
-#plt.xlabel('Day')
-#plt.show()
-
-# =============================================================================
-# TEST
-# =============================================================================
-#def TEST0():
-#    sample = np.random.normal(100, 16, 1000)
-#    normalTest(sample, 0.05) #without normalizing the sample
-#    sample_normalized = [0 for x in range(sample.size)]
-#    for i in range(0, sample.size):
-#         sample_normalized[i] = (sample[i]-100)/16
-#    normalTest(sample_normalized, 0.05) #with the sample normalized
-    
-#TEST0()
-    
-#with open('data.txt', 'w') as data_file: 
-#    data_file.write(str(prices))
-
-#print(df['Close'][1])
-
-
-
-#style.use('ggplot')
-#start = dt.datetime(2020, 1, 1)
-#end = dt.datetime.now()
-#df = web.DataReader("YPFD.BA", 'yahoo', start, end)
-#df['Close'].plot()
-#df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-#plt.show()
-#print(df.head())
